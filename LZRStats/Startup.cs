8e3a53709d0f4e03e6ab1c92c9ad86e687cde2e3
Trile33 <saletrile33@gmail.com>
2017-09-17 19:00:04 +0200

    Add project files.

---
+++ LZRStats/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LZRStats.Startup))]
+namespace LZRStats
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
