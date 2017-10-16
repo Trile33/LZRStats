4d2df6df58ae7a89ce8a3286f4cfc86e014e47a2
Trile33 <saletrile33@gmail.com>
2017-10-16 22:53:01 +0200

    PdfExtractor test

---
+++ LZRStats/PdfExtraction/PdfExtractor.cs
@@ -0,0 +1,20 @@
+﻿using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LZRStats.PdfExtraction
+{
+    public class PdfExtractor
+    {
+
+        public static string ExtractFromPdf(string PDFFilePath)
+        {
+            PDDocument doc = PDDocument.load(PDFFilePath);
+            PDFTextStripper stripper = new PDFTextStripper();
+            return stripper.getText(doc);
+        }
+    }
+}
