ac4ac3f3a0933537967452bc19887c81c70be07f
Trile33 <saletrile33@gmail.com>
2017-10-21 14:01:53 +0200

    Switched from pdf import to docx import. File import implemented.

---
+++ LZRStats/Controllers/PDFImportController.cs
@@ -0,0 +1,39 @@
+﻿using LZRStats.PdfExtraction;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.HtmlControls;
+
+namespace LZRStats.Controllers
+{
+    public class PDFImportController : Controller
+    {
+        protected HtmlInputFile file;
+        // GET: PDFImport
+        public ActionResult PDFImport()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PDFImport(HttpPostedFileBase file)
+        {
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the filename
+                var fileName = Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                var path = Path.Combine(Server.MapPath("~/PDFFiles"), fileName);
+                file.SaveAs(path);
+
+                PdfExtractor.ExtractFromPdf(path);
+            }
+            // redirect back to the index action to show the form once again
+            return RedirectToAction("PDFImport");
+        }
+    }
+}
--- LZRStats/PdfExtraction/PdfExtractor.cs
+++ LZRStats/PdfExtraction/PdfExtractor.cs
@@ -1,4 +1,7 @@
-﻿using org.apache.pdfbox.pdmodel;
+﻿using LZRStats.DAL;
+using LZRStats.Models;
+using Novacode;
+using org.apache.pdfbox.pdmodel;
 using org.apache.pdfbox.util;
 using System;
 using System.Collections.Generic;
@@ -9,12 +12,74 @@ namespace LZRStats.PdfExtraction
 {
     public class PdfExtractor
     {
+        private static DatabaseContext db = new DatabaseContext();
 
-        public static string ExtractFromPdf(string PDFFilePath)
+        public static void ExtractFromPdf(string filePath)
         {
-            PDDocument doc = PDDocument.load(PDFFilePath);
-            PDFTextStripper stripper = new PDFTextStripper();
-            return stripper.getText(doc);
+            DocX docx = DocX.Load(filePath);
+            CreateDataFromFile(docx.Text);
+
         }
+
+        public static void CreateDataFromFile(string data)
+        {
+            var withoutTabs = data.Replace('\t', ' ');
+            var lines = withoutTabs.Split('\n');
+            var gameData = lines[1].Replace("         ", " ").Split(' ');
+            var teamName = gameData[3];
+            var team = GetTeamForTeamName(teamName);
+            var players = new List<Player>();
+
+            
+           
+
+        }
+
+        private static Team GetTeamForTeamName(string teamName)
+        {
+            var team = db.Teams.Where(t => t.Name.Equals(teamName, StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            if (team == null)
+            {
+                team = new Team();
+                team.Name = teamName;
+            }
+            return team;
+        }
+
+        #region PlayerShootingStats
+        private static void ExtractPlayerShootingStats(Player player, string[] playerShooting)
+        {
+            if (playerShooting[1].Length == 1)
+            {
+                player.FG2Made = 0;
+                player.FG2Attempted = 0;
+            }
+            else
+            {
+                player.FG2Made = int.Parse(playerShooting[1].Split('/')[0]);
+                player.FG2Attempted = int.Parse(playerShooting[1].Split('/')[1]);
+            }
+            if (playerShooting[2].Length == 1)
+            {
+                player.FG3Made = 0;
+                player.FG3Attempted = 0;
+            }
+            else
+            {
+                player.FG3Made = int.Parse(playerShooting[2].Split('/')[0]);
+                player.FG3Attempted = int.Parse(playerShooting[2].Split('/')[1]);
+            }
+            if (playerShooting[3].Length == 1)
+            {
+                player.FTMade = 0;
+                player.FTAttempted = 0;
+            }
+            else
+            {
+                player.FTMade = int.Parse(playerShooting[3].Split('/')[0]);
+                player.FTAttempted = int.Parse(playerShooting[3].Split('/')[1]);
+            }
+        }
+        #endregion
     }
 }
